96e7b7d27ecb82e4a1daa7a48eea4e44fe0f3ac2
forex <forex@DESKTOP-9V4C0S1>
2017-02-21 17:15:27 +0800

    try to read line then cut token but not finish so still has bug

--- PL/src/PL_10220127/Main.java
+++ PL/src/PL_10220127/Main.java
@@ -3,8 +3,8 @@ package PL_10220127;
 import token.gettoken;
 
 public class Main {
-	public static void main(String []args){
-		gettoken G = new gettoken();
-		G.parser();
-	}
+    public static void main(String []args){
+        gettoken G = new gettoken();
+        G.parser();
+    }
 }
--- PL/src/token/gettoken.java
+++ PL/src/token/gettoken.java
@@ -3,34 +3,40 @@ package token;
 import java.util.Scanner;
 
 public class gettoken {
-	private int line;
-	private int column;
-	private Scanner scan;
-	public gettoken(){
-		this.line = 0;
-		this.column = 0;
-		this.scan = new Scanner(System.in);
-	}
-	private String getNext(){
-		String S;
-		while(this.scan.hasNext()){
-			S = this.scan.next();
-			if(S.compareTo(";") == 0){
-				if(this.scan.hasNext()){
-					this.scan.nextLine();
-				} else {
-					return null;
-				}
-			} else {
-				return S;
-			}
-		}
-		return null;
-	}
-	public void parser(){
-		String beSolved;
-		while((beSolved = this.getNext()) != null){
-			System.out.println(beSolved);
-		}
-	}
+    private int line;
+    private int column;
+    private Scanner scan;
+    public gettoken(){
+        this.line = 0;
+        this.column = 0;
+        this.scan = new Scanner(System.in);
+    }
+    private String getNext(){
+        String aLine;
+        String[] token;
+        while(this.scan.hasNext()){
+            aLine = this.scan.nextLine();
+            token = aLine.split(" ");
+            for(String S:token)
+                System.out.println(S);
+            if(aLine.compareTo(";") == 0){
+                if(this.scan.hasNext()){
+                    this.scan.nextLine();
+                } else {
+                    return null;
+                }
+            } else {
+                if(aLine.compareTo("EOF") == 0)
+                    System.exit(0);
+                return aLine;
+            }
+        }
+        return null;
+    }
+    public void parser(){
+        String beSolved;
+        while((beSolved = this.getNext()) != null){
+            System.out.println(beSolved + " " + this.line);
+        }
+    }
 }
